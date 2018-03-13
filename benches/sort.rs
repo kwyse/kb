4021daaea6cb6289101c5c495406809f7f1e2f65
Krishan Wyse <kwysek@gmail.com>
2018-03-13 20:18:11 +0000

    Use same input across benchmarks

---
+++ benches/sort.rs
@@ -0,0 +1,41 @@
+//! Insertion sort benchmarks
+
+#![feature(test)]
+
+#[macro_use]
+extern crate lazy_static;
+
+extern crate kb;
+extern crate rand;
+extern crate test;
+
+use kb::sort::insertion::*;
+
+use rand::Rng;
+use test::Bencher;
+
+lazy_static! {
+    static ref INPUT: Vec<u8> = gen_u8_vec(1_000);
+}
+
+#[bench]
+fn insertion_sort_clrs_1000_u8(b: &mut Bencher) {
+    let mut arr = INPUT.clone();
+    b.iter(|| clrs(&mut arr));
+}
+
+#[bench]
+fn insertion_sort_rev_1000_u8(b: &mut Bencher) {
+    let mut arr = INPUT.clone();
+    b.iter(|| rev(&mut arr));
+}
+
+fn gen_u8_vec(n: usize) -> Vec<u8> {
+    let mut rng = ::rand::thread_rng();
+    let mut vec = Vec::with_capacity(n);
+    for _ in 0..n {
+        vec.push(rng.gen());
+    }
+
+    vec
+}
