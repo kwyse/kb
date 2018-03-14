bdff1d2824a4c5fd3cd73e4f9366e0fecbe1f2fb
Krishan Wyse <kwysek@gmail.com>
2018-03-14 20:04:22 +0000

    Add details of linear search loop invariant

--- src/search/linear.rs
+++ src/search/linear.rs
@@ -1,8 +1,25 @@
 //! Linear search
+//!
+//! Searches for an element in O(n) time.
 
-/// My own implementation
+/// My own implementation of CLRS exercise 2.1-3
 ///
-/// CLRS exercise 2.1-3
+/// Iterates through a slice until a target value is found. If the value is
+/// found, `Some(index)` is returned, otherwise `None` is returned.
+///
+/// # Loop invariant
+///
+/// The target value must have been found, or we keep searching until we have
+/// compared every element.
+///
+/// *Initialization*: The value has not been found, so we continue the search.
+///
+/// *Maintenance*: For each iteration, we check if the value has been found. If
+/// it has, we return the index at which it appears, otherwise we proceed to
+/// the next iteration.
+///
+/// *Termination*: If we found the value, its index is returned. If not, we
+/// have exhausted the search.
 pub fn linear<T>(target: &T, values: &[T]) -> Option<usize>
 where
     T: PartialEq
