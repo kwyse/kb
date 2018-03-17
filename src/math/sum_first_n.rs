35d2118920bc27dda84c4671e527bebdacfaba1f
Krishan Wyse <kwysek@gmail.com>
2018-03-17 14:25:18 +0000

    State summing to n is also known as a partial sum

--- src/math/sum_first_n.rs
+++ src/math/sum_first_n.rs
@@ -1,6 +1,9 @@
 //! Sum the first n numbers
 
 /// My own implementation of [this proof](https://math.stackexchange.com/a/2288)
+///
+/// This is also known as finding the _nth partial sum_.The return value is
+/// always a triangular number.
 pub fn sum_first_n(n: u64) -> u64 {
     (n * (n + 1)) / 2
 }
