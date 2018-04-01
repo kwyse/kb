bd417d2c3e6397d15a8729d287329b78a662420f
Krishan Wyse <kwysek@gmail.com>
2018-04-01 09:41:22 +0100

    Add DFS in-order recursive function

--- src/tree/traversal/dfs_in_order.rs
+++ src/tree/traversal/dfs_in_order.rs
@@ -1,11 +1,11 @@
 //! Depth-first, in-order tree traversal
+//!
+//! If traversing a binary search tree, `visit` will yield nodes in sorted
+//! order.
 
 use super::super::BinaryNode;
 
 /// Traverses the nodes iteratively
-///
-/// If traversing a binary search tree, `visit` will yield nodes in sorted
-/// order.
 pub fn traverse_iterative<'a, T, F>(root: &BinaryNode<'a, T>, mut visit: F)
 where
     F: FnMut(&BinaryNode<T>)
@@ -21,6 +21,18 @@ where
             visit(inner);
             node = inner.right;
         }
+    }
+}
+
+/// Traverses the nodes recursively
+pub fn traverse_recursive<'a, T, F>(root: Option<&BinaryNode<'a, T>>, visit: &mut F)
+where
+    F: FnMut(&BinaryNode<T>)
+{
+    if let Some(node) = root {
+        traverse_recursive(node.left, visit);
+        visit(node);
+        traverse_recursive(node.right, visit);
     }
 }
 
@@ -40,6 +52,26 @@ mod tests {
 
         let mut traversed_nodes = Vec::with_capacity(7);
         traverse_iterative(&four, |node| traversed_nodes.push(node.data));
+        assert_eq!(traversed_nodes, vec![1, 2, 3, 4, 5, 6, 7]);
+    }
+
+    #[test]
+    fn test_traversal_recursive() {
+        let mut traversed_nodes = Vec::with_capacity(7);
+        let one = BinaryNode { data: 1, left: None, right: None };
+
+        traverse_recursive(Some(&one), &mut |node| traversed_nodes.push(node.data));
+        assert_eq!(traversed_nodes, vec![1]);
+
+        traversed_nodes.clear();
+        let three = BinaryNode { data: 3, left: None, right: None };
+        let two = BinaryNode { data: 2, left: Some(&one), right: Some(&three) };
+        let five = BinaryNode { data: 5, left: None, right: None };
+        let seven = BinaryNode { data: 7, left: None, right: None };
+        let six = BinaryNode { data: 6, left: Some(&five), right: Some(&seven) };
+        let four = BinaryNode { data: 4, left: Some(&two), right: Some(&six) };
+
+        traverse_recursive(Some(&four), &mut |node| traversed_nodes.push(node.data));
         assert_eq!(traversed_nodes, vec![1, 2, 3, 4, 5, 6, 7]);
     }
 }
