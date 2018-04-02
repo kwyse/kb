e24c2965c8b824298aabeaa0eba9532eb5aeb591
Krishan Wyse <kwysek@gmail.com>
2018-04-02 17:45:20 +0100

    Add CLRS 2.3.2 details on merge sort recurrence
    Also add exercise 2.3-1 solution.

--- src/sort/merge.rs
+++ src/sort/merge.rs
@@ -18,6 +18,98 @@
 //!
 //! Merge sort is called recursively until the subsequence to be sorted has a
 //! length of *one*, in which case it is sorted.
+//!
+//! # Recurrence
+//!
+//! A recurrence of a divide and conquer problem, _T(n)_ of input size _n_, is
+//! defined by:
+//!
+//! ```text
+//! O(1)                    # if n ≤ c
+//! aT(n/b) + D(n) + C(n)   # otherwise
+//! ```
+//!
+//! where _c_ is a constant that is small enough that the solution is
+//! straightforward and it takes constant time, _a_ is the number of
+//! subproblems when we divide the problem, each _1/b_ the size of the original,
+//! _D(n)_ is the time taken to divide the problem into subproblems, and _C(n)_
+//! is the time taken to combine the solutions to the subproblems into the
+//! original problem.
+//!
+//! For merge sort, both _a_ and _b_ are 2, the divide step is a middle index
+//! calculation that takes constant time, and the combine step takes _O(n)_ time
+//! because because it has to traverse the subarray. _O(n) + O(1) = O(n)_, and
+//! the conquer step recursively solves two subproblems of size _n/2_, so it
+//! contributes _2T(n/2)_. A merge of one element is done in constant time.This
+//! gives the recurrence for merge sort:
+//!
+//! ```text
+//! O(1)            # if n = 1
+//! 2T(n/2) + O(n)  # if n > 1
+//! ```
+//!
+//! The recurrence can prove merge sort has a running time of _O(n lg(n))_. Let
+//! _c_ be the time required to solve problems of size 1 and let us assume that
+//! _n_ is a power of 2. We can rewrite the recurrence as:
+//!
+//! ```text
+//! c               # if n = 1
+//! 2T(n/2) + cn    # if n > 1
+//! ```
+//!
+//! ![Merge sort
+//! recurrence](http://algoparc.ics.hawaii.edu/~nodari/teaching/f15/Notes/Topic-02/recurrence-tree-mergesort-3.jpg)
+//!
+//! The recursion tree shows that the cost of each level is _cn_. If _n_ is the
+//! number of leaves on the tree, then _lg(n) + 1_ is the depth of tree:
+//!
+//! ```text
+//! n = 1 => lg(1) + 1 = 1
+//! n = 2 => lg(2) + 1 = 2
+//! n = 4 => lg(4) + 1 = 3
+//! ...
+//! ```
+//!
+//! If we have _2ⁱ_ leaves, we get _lg(2ⁱ) + 1 = i + 1_. The next input size
+//! to consider, _2ⁱ⁺¹_, has one more level in its recursion tree, so its
+//! number of leaves is _lg(2ⁱ⁺¹) + 1 = (i + 1) + 1_.
+//!
+//! If we have _lg(n) + 1_ levels, each costing _cn_, the total cost is
+//! _cn(lg(n) + 1) = cn(lg(n)) + cn_. Ignoring the constant factor and the
+//! low-order term, we get _O(n lg(n))_.
+//!
+//! # Bottom-up progression
+//!
+//! Merge sort on an input array of ⟨3, 41, 52, 26, 38, 57, 9, 49⟩.
+//!
+//! ```text
+//! +--------+-------+-------+--------+--------+-------+-------+--------+
+//! |        |       |       |        |        |       |       |        |
+//! |    3   |    9  |   26  |   38   |   41   |   49  |   52  |   57   |
+//! |        |       |       |        |        |       |       |        |
+//! +--------+-------+-^-----+--------+--------+-----^-+-------+--------+
+//!                    |                             |
+//!                    |                             |
+//! +-------+-------+--+----+-------+   +-------+----+--+-------+-------+
+//! |       |       |       |       |   |       |       |       |       |
+//! |   3   |  26   |  41   |  52   |   |   9   |   38  |   49  |   57  |
+//! |       |       |       |       |   |       |       |       |       |
+//! +-------+--^----+----^--+-------+   +-------+--^----+----^--+-------+
+//!            |         |                         |         |
+//!            |         |                         |         |
+//! +------+---+--+   +--+---+------+   +------+---+--+   +--+---+------+
+//! |      |      |   |      |      |   |      |      |   |      |      |
+//! |   3  |  41  |   |  26  |  52  |   |  38  |  57  |   |   9  |  49  |
+//! |      |      |   |      |      |   |      |      |   |      |      |
+//! +--^---+---^--+   +--^---+---^--+   +--^---+---^--+   +--^---+---^--+
+//!    |       |         |       |         |       |         |       |
+//!    |       |         |       |         |       |         |       |
+//! +--+-+   +-+--+   +--+-+   +-+--+   +--+-+   +-+--+   +--+-+   +-+--+
+//! |    |   |    |   |    |   |    |   |    |   |    |   |    |   |    |
+//! |  3 |   | 41 |   | 52 |   | 26 |   | 38 |   | 57 |   |  9 |   | 49 |
+//! |    |   |    |   |    |   |    |   |    |   |    |   |    |   |    |
+//! +----+   +----+   +----+   +----+   +----+   +----+   +----+   +----+
+//! ```
 
 /// The solution presented in CLRS
 pub fn clrs_merge_sort(mut values: &mut [f64], p: usize, r: usize) {
