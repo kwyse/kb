d4bfaa204e4d1889d6ee7dc4ef0e2efd3e1138bb
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-04-03 19:59:52 +0000

    Deploy kwyse/kb to github.com/kwyse/kb.git:gh-pages

--- kb/sort/merge/sidebar-items.js
+++ kb/sort/merge/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["clrs_merge","CLRS version of the merge operation used by merge sort"],["clrs_merge_sort","The solution presented in CLRS"]]});
+initSidebarItems({"fn":[["clrs_merge","CLRS version of the merge operation used by merge sort"],["clrs_merge_sort","The solution presented in CLRS"],["krw_merge_sort","My own implementation of merge sort"]]});
